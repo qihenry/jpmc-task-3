4ce52893a42bdcbcfc3f80f2d6c733e63d0e875b
qihenry <henryqi301@gmail.com>
2022-10-08 22:17:00 -0400

    implemented task 3

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,32 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
+  price_abc: number,
+  price_def: number,
+  ratio: number,
+  upper_bound: number,
+  lower_bound: number,
   timestamp: Date,
+  trigger_alert: number | undefined,
 }
 
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+  static generateRow(serverRespond: ServerRespond[]) {
+    const ABC = (serverRespond[0].top_ask.price + serverRespond[0].top_bid.price)/2;
+    const DEF = (serverRespond[1].top_ask.price + serverRespond[1].top_bid.price)/2;
+    const ratio = ABC/DEF;
+    const upperB = 1.01;
+    const lowerB = .99;
+    return  {
+      price_abc: ABC,
+      price_def: DEF,
+      ratio,
+      timestamp: serverRespond[0].timestamp > serverRespond[1].timestamp ?
+        serverRespond[0].timestamp : serverRespond[1].timestamp,
+      upper_bound: upperB,
+      lower_bound: lowerB,
+      trigger_alert: (ratio > upperB || ratio <lowerB) ? ratio : undefined,  
+    };
   }
 }
